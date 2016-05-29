93c317cd3639dabfff29e5be8a29307cd0d3fee9
narik121 <sreeharikiran@gmail.com>
2016-05-30 00:53:12 +0530

    Javascript update to cointoss with bootstrap update

--- toss.js
+++ toss.js
@@ -4,6 +4,9 @@ var	tailcount = 1;
 var won = 0;
 var lost = 0;
 
+var wonbyHeads = 0;
+var wonbyTails = 0;
+
 function heads (){
 	var toss_result = flip();
 	
@@ -25,8 +28,8 @@ function toss(result,coin) {
 		var coin = coin;
 		var div_field = document.getElementById("result");
 		
-		var head_filed = document.getElementById("heads");
-		var tail_filed = document.getElementById("tails");
+		var head_filed = document.getElementById("headsCount");
+		var tail_filed = document.getElementById("tailsCount");
 		
 		var stats_won = document.getElementById("stats_won");
 		var stats_lost = document.getElementById("stats_lost");
@@ -35,19 +38,21 @@ function toss(result,coin) {
 		
 		var image_field = document.getElementById("image");
 		
-		image_field.innerHTML = "<img src = \".\\Coin.gif\"/>";
+		//image_field.innerHTML = "<img src = \".\\Coin.gif\"/>";
 		
 		if(output_result) {
 			
 			coin ? (head_filed.innerHTML = headCount++) : (tail_filed.innerHTML = tailcount++);
-			div_field.innerHTML = " You Won!";
-			stats_won.innerHTML = ++won;
+			div_field.innerHTML = "<div class = \"alert alert-success\"><strong><h3 class=\"text-center\">You Won!</h3></strong></div>";
+			++won;
+			stats_won.innerHTML = "Won : "+won;
 		}
 		
 		else {
 			coin ? (tail_filed.innerHTML = tailcount++) : (head_filed.innerHTML = headCount++);			
-			div_field.innerHTML = "You Lost!";
-			stats_lost.innerHTML = ++lost;
+			div_field.innerHTML = "<div class = \"alert alert-danger\"><strong><h3 class=\"text-center\">You Lost!</h3></strong> </div>";
+			++lost;
+			stats_lost.innerHTML = "Lost : " +lost;
 		}
 }
 
